aeff9dd015ede81b60a2301b3fa578a024d423c4
Tankred Hase <mail@tankredhase.de>
2020-04-15 16:43:54 +0800

    Remove support for watch-only wallets

--- src/class/index.js
+++ src/class/index.js
@@ -6,7 +6,6 @@ export * from './segwit-p2sh-wallet';
 export * from './hd-segwit-p2sh-wallet';
 export * from './hd-legacy-breadwallet-wallet';
 export * from './hd-legacy-p2pkh-wallet';
-export * from './watch-only-wallet';
 export * from './abstract-hd-wallet';
 export * from './hd-segwit-bech32-wallet';
 export * from './hd-segwit-bech32-transaction';
