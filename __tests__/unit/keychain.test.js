d58c716e1743c91879e3ecf4fa1ff78d964105f0
Tankred Hase <mail@tankredhase.de>
2020-04-20 16:38:05 +0800

    Implement and test keychain module

---
+++ __tests__/unit/keychain.test.js
@@ -0,0 +1,49 @@
+import * as RNKeychain from 'react-native-keychain';
+import * as keychain from '../../src/keychain';
+
+describe('Keychain unit test', () => {
+  beforeEach(() => {
+    RNKeychain._nuke();
+    RNKeychain.setInternetCredentials.mockClear();
+    RNKeychain.getInternetCredentials.mockClear();
+  });
+
+  afterAll(() => {
+    RNKeychain._nuke();
+  });
+
+  describe('setItem', () => {
+    it('should fail on invalid args', async () => {
+      await expect(keychain.setItem()).rejects.toThrow(/Invalid/);
+    });
+
+    it('should fail on api error', async () => {
+      RNKeychain.setInternetCredentials.mockRejectedValueOnce(new Error('boom'));
+      await expect(keychain.setItem('some-key', 'some-value')).rejects.toThrow('boom');
+    });
+
+    it('should store item and fetch correctly', async () => {
+      await keychain.setItem('some-key', 'some-value');
+      const val = await keychain.getItem('some-key');
+      expect(val).toBe('some-value');
+      expect(RNKeychain.setInternetCredentials).toHaveBeenCalledWith('0_some-key', 'photonlib', 'some-value', { accessible: 'wutdo' });
+      expect(RNKeychain.getInternetCredentials).toHaveBeenCalledWith('0_some-key');
+    });
+  });
+
+  describe('getItem', () => {
+    it('should fail on invalid args', async () => {
+      await expect(keychain.getItem()).rejects.toThrow(/Invalid/);
+    });
+
+    it('should fail on api error', async () => {
+      RNKeychain.getInternetCredentials.mockRejectedValueOnce(new Error('boom'));
+      await expect(keychain.getItem('some-key')).rejects.toThrow('boom');
+    });
+
+    it('should return null if not found', async () => {
+      const val = await keychain.getItem('some-key');
+      expect(val).toBe(null);
+    });
+  });
+});
---
+++ src/keychain.js
@@ -0,0 +1,43 @@
+/**
+ * @fileOverview a module to wrap native secure key store apis
+ */
+
+import * as RNKeychain from 'react-native-keychain';
+
+const VERSION = '0';
+const USER = 'photonlib';
+
+/**
+ * Store an item in the keychain.
+ * @param {string} key   The key by which to do a lookup
+ * @param {string} value The value to be stored
+ * @return {Promise<undefined>}
+ */
+export async function setItem(key, value) {
+  if (!key || !value) {
+    throw new Error('Invalid args');
+  }
+  const options = {
+    accessible: RNKeychain.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+  };
+  const vKey = `${VERSION}_${key}`;
+  await RNKeychain.setInternetCredentials(vKey, USER, value, options);
+}
+
+/**
+ * Read an item stored in the keychain.
+ * @param  {string} key      The key by which to do a lookup.
+ * @return {Promise<string>} The stored value
+ */
+export async function getItem(key) {
+  if (!key) {
+    throw new Error('Invalid args');
+  }
+  const vKey = `${VERSION}_${key}`;
+  const credentials = await RNKeychain.getInternetCredentials(vKey);
+  if (credentials) {
+    return credentials.password;
+  } else {
+    return null;
+  }
+}
