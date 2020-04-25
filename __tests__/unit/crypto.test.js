e79b315177aaaf9d7e17dc11fd86c17c059e2e2a
Tankred Hase <mail@tankredhase.de>
2020-04-25 10:40:27 +0800

    Concat iv + ciphertext for encrypt result

--- __tests__/unit/crypto.test.js
+++ __tests__/unit/crypto.test.js
@@ -1,16 +1,17 @@
 import nodeCrypto from 'crypto';
+import { randomBytes } from '../../src/random';
 import { Crypto } from '../../';
 
+const IV_LEN = Crypto.IV_LEN;
 const TAG_LEN = Crypto.TAG_LEN;
 
 describe('Crypto unit test', () => {
   describe('encrypt', () => {
     it('be decodable by node api', async () => {
       const key = await Crypto.generateKey();
-      const iv = await Crypto.generateIV();
       const pt = Buffer.from('secret stuff', 'utf8');
-      const ct = await Crypto.encrypt(pt, key, iv);
-      const decrypted = nodeDecrypt(ct, key, iv);
+      const ct = await Crypto.encrypt(pt, key);
+      const decrypted = await nodeDecrypt(ct, key);
       expect(decrypted.toString('utf8')).toBe('secret stuff');
     });
   });
@@ -18,24 +19,26 @@ describe('Crypto unit test', () => {
   describe('decrypt', () => {
     it('decode node api ciphertext', async () => {
       const key = await Crypto.generateKey();
-      const iv = await Crypto.generateIV();
       const pt = Buffer.from('secret stuff', 'utf8');
-      const ct = nodeEncrypt(pt, key, iv);
-      const decrypted = await Crypto.decrypt(ct, key, iv);
+      const ct = await nodeEncrypt(pt, key);
+      const decrypted = await Crypto.decrypt(ct, key);
       expect(decrypted.toString('utf8')).toBe('secret stuff');
     });
   });
 });
 
-function nodeEncrypt(pt, key, iv, adata = Buffer.alloc(0)) {
+async function nodeEncrypt(pt, key) {
+  const iv = await randomBytes(IV_LEN);
   const en = nodeCrypto.createCipheriv('aes-256-gcm', key, iv);
-  en.setAAD(adata);
-  return Buffer.concat([en.update(pt), en.final(), en.getAuthTag()]);
+  en.setAAD(Buffer.alloc(0));
+  return Buffer.concat([iv, en.update(pt), en.final(), en.getAuthTag()]);
 }
 
-function nodeDecrypt(ct, key, iv, adata = Buffer.alloc(0)) {
+async function nodeDecrypt(ciphertext, key) {
+  const iv = ciphertext.slice(0, IV_LEN);
+  const ct = ciphertext.slice(IV_LEN);
   const de = nodeCrypto.createDecipheriv('aes-256-gcm', key, iv);
-  de.setAAD(adata);
+  de.setAAD(Buffer.alloc(0));
   de.setAuthTag(ct.slice(ct.length - TAG_LEN, ct.length));
   return Buffer.concat([de.update(ct.slice(0, ct.length - TAG_LEN)), de.final()]);
 }
--- src/crypto.js
+++ src/crypto.js
@@ -1,7 +1,6 @@
 import { AES_GCM } from 'asmcrypto.js';
 import { randomBytes } from './random';
 
-export const ALGO = 'AES-256-GCM';
 export const KEY_LEN = 32; // size of the key in bytes
 export const IV_LEN = 12; // size of the IV in bytes
 export const TAG_LEN = 16; // size of the tag in bytes
@@ -10,58 +9,41 @@ export const TAG_LEN = 16; // size of the tag in bytes
  * Encrypt a plaintext using AES-256-GCM (authenticated encryption)
  * @param  {Buffer} plaintext  The plaintext bytes
  * @param  {Buffer} key        The symmetric encryption key
- * @param  {Buffer} iv         The initialisation vector
- * @return {Promise<Buffer>}   The encrypted ciphertext
+ * @return {Promise<Buffer>}   The iv + encrypted ciphertext
  */
-export async function encrypt(plaintext, key, iv) {
-  if (!Buffer.isBuffer(plaintext) || !Buffer.isBuffer(key) || key.length !== KEY_LEN || !Buffer.isBuffer(iv) || iv.length !== IV_LEN) {
+export async function encrypt(plaintext, key) {
+  if (!Buffer.isBuffer(plaintext) || !Buffer.isBuffer(key) || key.length !== KEY_LEN) {
     throw new Error('Invalid args');
   }
+  const iv = await randomBytes(IV_LEN);
   const p = new Uint8Array(plaintext);
   const k = new Uint8Array(key);
   const i = new Uint8Array(iv);
   const adata = new Uint8Array();
   const ct = AES_GCM.encrypt(p, k, i, adata, TAG_LEN);
-  return Promise.resolve(Buffer.from(ct.buffer));
+  return Buffer.concat([iv, Buffer.from(ct.buffer)]);
 }
 
 /**
  * Decrypt a ciphertext using AES-256-GCM (authenticated encryption)
- * @param  {Buffer} ciphertext  The ciphertext bytes
+ * @param  {Buffer} ciphertext  The iv + ciphertext bytes
  * @param  {Buffer} key         The symmetric encryption key
- * @param  {Buffer} iv          The initialisation vector
  * @return {Promise<Buffer>}    The decrypted plaintext
  */
-export async function decrypt(ciphertext, key, iv) {
-  if (!Buffer.isBuffer(ciphertext) || !Buffer.isBuffer(key) || key.length !== KEY_LEN || !Buffer.isBuffer(iv) || iv.length !== IV_LEN) {
+export async function decrypt(ciphertext, key) {
+  if (!Buffer.isBuffer(ciphertext) || !Buffer.isBuffer(key) || key.length !== KEY_LEN) {
     throw new Error('Invalid args');
   }
-  const c = new Uint8Array(ciphertext);
+  const iv = ciphertext.slice(0, IV_LEN);
+  const ct = ciphertext.slice(IV_LEN);
+  const c = new Uint8Array(ct);
   const k = new Uint8Array(key);
   const i = new Uint8Array(iv);
   const adata = new Uint8Array();
   const pt = AES_GCM.decrypt(c, k, i, adata, TAG_LEN);
-  return Promise.resolve(Buffer.from(pt.buffer));
+  return Buffer.from(pt.buffer);
 }
 
 export async function generateKey() {
   return randomBytes(KEY_LEN);
-}
-
-export async function generateIV() {
-  return randomBytes(IV_LEN);
-}
-
-export function toWireFormat(ciphertext, iv) {
-  return {
-    ct: ciphertext.toString('base64'),
-    iv: iv.toString('base64'),
-  };
-}
-
-export function parseWireFormat({ ct, iv }) {
-  return {
-    ciphertext: Buffer.from(ct, 'base64'),
-    iv: Buffer.from(iv, 'base64'),
-  };
 }
