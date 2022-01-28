4b24e76845949acf7cd3bbb533dba9b75d58b7a9
BitcoinZavior <BitcoinZavior@gmail.com>
2022-01-28 18:06:04 -0500

    add dotenv to dependency and setup config

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,5 +1,7 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
+require('dotenv').config();
+
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('@photon-sdk/react-native-icloudstore', () => mockAsyncStorage);
